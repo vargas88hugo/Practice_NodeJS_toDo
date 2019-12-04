406181d957909d5fd985c7a3a2577152b58306ee
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-04 11:37:29 -0500

    [update] added printed messages and information of json file in list option

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
  // Imports
 const argv = require('./config/yargs').argv;
 const toDo = require('./to-do/to-do');
+const colors = require('colors');
 
 
 let command = argv._[0];
@@ -16,7 +17,14 @@ switch( command ) {
         console.log(task);
         break;
     case 'list':
-        console.log('list');
+        let list = toDo.getList();
+        
+        for (let task of list) {
+            console.log('================= To Do ================='.green);
+            console.log(task.description + " - " + task.completed);
+            console.log('=========================================\n'.green);
+        }
+        
         break;
     case 'update':
         console.log('update');
