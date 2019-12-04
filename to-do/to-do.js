13b23b5aca6b0f3e0ed61210401fb6581d22af6e
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-04 11:40:17 -0500

    [update] created methods for store and load data

--- to-do/to-do.js
+++ to-do/to-do.js
@@ -2,18 +2,65 @@ const fs = require('fs');
 
 let listToDo = [];
 
-// Function that creates an object from the arguments
+/**
+ * Function that stores data in a file with the method writeFile
+ */
+const storeDB = () => {
+    let data = JSON.stringify(listToDo);
+
+    fs.writeFile('db/data.json', data, (e) => {
+        if (e) throw new Error('Cannot be stored');
+    });
+}
+
+/**
+ * Function that loads the information of json files. It
+ * creates persistence in the data
+ */ 
+const loadDB = () => {
+    try {
+        listToDo = require('../db/data.json');
+    } catch (error) {
+        listToDo = [];
+    }
+}
+
+/**
+ * Function that creates an object from the arguments
+ * @param {String} description - argument of the command line
+ */
 const create = (description) => {
+    loadDB();
+
     let toDo = {
         description,
         completed: false
     }
 
-    listToDo.push('toDo');
+    listToDo.push(toDo);
+
+    storeDB();
 
     return toDo;
 }
 
+/**
+ * Function that reads a json file and store it in an array
+ * @return {Array} array with json objects
+ */
+const getList = () => {
+    try {
+        listToDo = require('../db/data.json');
+    } catch (error) {
+        listToDo = [];
+    }
+
+    return listToDo;
+}
+
+
+
 module.exports = {
-    create
+    create,
+    getList
 }
