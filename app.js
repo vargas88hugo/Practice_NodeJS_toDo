954612c66c95e1029334e992858dfad5d54f0c28
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-06 13:45:34 -0500

    [Feature] added update and delete options, these options are already connected with the logic

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ switch( command ) {
         let task = toDo.createList(argv.description);
 
         if (task != null) {
-            console.log(task);
+            console.log(('The task ' + task.description + ' has been created.\n').green);
         } else {
-            console.log('Description already exists please enter another\n'.red);
+            console.log('Description already exists please enter another.\n'.red);
         }
         break;
     case 'list':
@@ -32,7 +32,22 @@ switch( command ) {
         
         break;
     case 'update':
-        toDo.updateList(argv.description, argv.complete);
+        let foo = toDo.updateList(argv.description, argv.complete);
+        if (foo == true) {
+            console.log(('The task ' + argv.description + ' has been updated.\n'). green);
+        } else {
+            console.log('Description not found.'.red);
+        }
+        break;
+    case 'delete':
+        let dele = toDo.deleteObject(argv.description);
+
+        if (dele == true) {
+            console.log(("The task " + argv.description + " has been deleted.\n").green);
+        } else {
+            console.log("Description not found.".red);
+        }
+
         break;
     default:
         console.log('unrecognized command');
